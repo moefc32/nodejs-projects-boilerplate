d2f47277069e34cf6b3f952eccc05f86f80eda1d
Faizal Chan <moefc32@gmail.com>
2023-04-28 15:47:09 +0700

    remove unnecessary files

--- view/js/script.js
+++ view/js/script.js
@@ -1,18 +1,1 @@
 'use strict';
-
-const toast = new bootstrap.Toast(document.querySelector('#toast'));
-const toastMessage = document.querySelector('#toast-message');
-
-UpUp.start({
-    'content-url': '/html/offline.html',
-    'assets': [
-        '/img/favicon.svg',
-        '/css/fontawesome.min.css',
-        '/css/style.css',
-    ],
-});
-
-function triggerToast(message) {
-    toastMessage.innerText = message;
-    toast.show();
-}
