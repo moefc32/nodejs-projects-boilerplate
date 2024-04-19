86efa76a3fc714ca252375689341234dfd10d6a9
Faizal Chan <moefc32@gmail.com>
2024-04-19 18:39:35 +0700

    async...await

--- controller/index.js
+++ controller/index.js
@@ -3,21 +3,21 @@ const model = require('../model');
 
 module.exports = {
     healthCheck: async (req, res) => {
-        return res.status(200).send({
+        return await res.status(200).send({
             application: APP_NAME,
             message: 'Application is healthy.',
         })
     },
     checkData: async (req, res) => {
-        return res.status(200).send({
+        return await res.status(200).send({
             application: APP_NAME,
             message: model.data,
         })
     },
     root: async (req, res) => {
-        return res.view('./view/html/index.ejs', { app_name: APP_NAME });
+        return await res.view('./view/html/index.ejs', { app_name: APP_NAME });
     },
-    notFound: async function (req, res) {
+    notFound: async (req, res) => {
         return await res.status(404).view('./view/html/notfound.ejs', { app_name: APP_NAME });
     },
 }
